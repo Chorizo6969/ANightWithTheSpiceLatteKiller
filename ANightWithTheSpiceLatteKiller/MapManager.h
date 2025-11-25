f041412f0fff74ec199bfcdc9cefa4ed829f165c
JElouann <eljouenne@gmail.com>
2025-11-25 09:23:30 +0100

    DOING - Map

--- ANightWithTheSpiceLatteKiller/MapManager.h
+++ ANightWithTheSpiceLatteKiller/MapManager.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <vector>
+#include "PlayerMain.h"
+#include "ConsolePrinter.h"
+
+using namespace std;
+
 class MapManager
 {
+public:
+	MapManager(PlayerMain* player, ConsolePrinter* printer);
+	pair<float, float> PlayerPosition;
+	void PrintMap(int colorOverrideIndex = -1, bool excludePlayer = false);
+	void TintMap(int colorIndex, int timeMiliSec, bool excludePlayer = false);
+
+private:
+	PlayerMain* _player;
+	ConsolePrinter* _printer; 
+	vector<vector<char>> _map;
+	vector<CHAR_INFO> _buffer;
 };
 

8801bfca1c0e8c4d687e8ed53c01d2521787b147
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-11-25 09:33:40 +0100

    Header Player

--- ANightWithTheSpiceLatteKiller/PlayerInteraction.h
+++ ANightWithTheSpiceLatteKiller/PlayerInteraction.h
@@ -1,5 +1,12 @@
+#include "Interactable.h"
+#include "PlayerMain.h"
+
 #pragma once
-class PlayerInteraction
-{
+class PlayerInteraction{
+public:
+	Interactable currentObjectInteract;
+	PlayerMain PlayerMainRef;
+
+	void Interact();
 };
 

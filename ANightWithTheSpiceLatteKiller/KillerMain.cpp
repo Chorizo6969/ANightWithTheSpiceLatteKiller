3e663f50456b2ff3069f6af59cf92153b576c133
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-11-25 15:15:58 +0100

    Observer Clean

--- ANightWithTheSpiceLatteKiller/KillerMain.cpp
+++ ANightWithTheSpiceLatteKiller/KillerMain.cpp
@@ -18,37 +18,22 @@ void KillerMain::KillerDo() {
 void KillerMain::DebugStateMachine() {
 	KillerBrainRef->SwitchState(KillerBrainRef->stateGoKill);
 	KillerDo();
-
-
 }
 
 void KillerMain::ClientCode() { //Exemple d'un évent
 	EventManager* subject = new EventManager;
 	StepCounter* observer1 = new StepCounter(*subject);
-	StepCounter* observer2 = new StepCounter(*subject);
-	StepCounter* observer3 = new StepCounter(*subject);
-	StepCounter* observer4;
-	StepCounter* observer5;
+	//StepCounter* observer2 = new StepCounter(*subject);
 
-	subject->CreateMessage("Hello World! :D");
-	observer3->RemoveMeFromTheList();
+	subject->CreateMessage(to_string(observer1->PlayerStep)); //Print et change le message
 
 	subject->CreateMessage("The weather is hot today! :p");
-	observer4 = new StepCounter(*subject);
 
-	observer2->RemoveMeFromTheList();
-	observer5 = new StepCounter(*subject);
+	//observer2->RemoveMeFromTheList();
 
-	subject->CreateMessage("My new car is great! ;)");
-	observer5->RemoveMeFromTheList();
-
-	observer4->RemoveMeFromTheList();
 	observer1->RemoveMeFromTheList();
 
-	delete observer5;
-	delete observer4;
-	delete observer3;
-	delete observer2;
+	//delete observer2;
 	delete observer1;
 	delete subject;
 }

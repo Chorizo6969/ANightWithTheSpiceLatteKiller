5879893f6ee033664da1d0197b92259b06886038
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-11-28 19:15:01 +0100

    fix

--- ANightWithTheSpiceLatteKiller/PlayerInteraction.cpp
+++ ANightWithTheSpiceLatteKiller/PlayerInteraction.cpp
@@ -1,15 +1,15 @@
 #include "PlayerInteraction.h"
 
-void PlayerInteraction::Interact(char character)
+void PlayerInteraction::Interact()
 {
 	//MapManager -> CharPosMapByRoom() Il prends un character et renvoit la liste des pos au sol
 	//MapManager-> Map[la position.second][la position.first]
 
 	//Si le joueur est sur un point d'exclamation, envoit dialogue + cin E
-	if (character == '!')
-	{
-		std::cout << "Le joueur est devant une porte..." << std::endl;
-		std::cout << "Appuyer sur E pour l'ouvrir" << std::endl;
-		//porte
-	}
+	//if (character == '!')
+	//{
+	//	std::cout << "Le joueur est devant une porte..." << std::endl;
+	//	std::cout << "Appuyer sur E pour l'ouvrir" << std::endl;
+	//	//porte
+	//}
 };
